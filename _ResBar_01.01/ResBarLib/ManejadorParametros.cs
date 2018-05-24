540c0bbac47578268ab01bcd7d59d11614256a8f
Ivan Portillo <ivan.portillo96@hotmail.com>
2018-05-23 21:44:44 -0600

    ManejadorParametro Terminado

--- _ResBar_01.01/ResBarLib/ManejadorParametros.cs
+++ _ResBar_01.01/ResBarLib/ManejadorParametros.cs
@@ -13,11 +13,12 @@ namespace ResBarLib
 {
     public static class ManejadorParametros
     {
+        //Va a la base de datos y obtiene todos los parametros que están en dicha tabla, devolviendo una colección de objetos parametros.
         public static List<parametro> Obtener()
         {
             try
             {
-                using (IDbConnection db = new MySqlConnection(ConfigurationManager.ConnectionStrings["cn"].ConnectionString))
+                using (IDbConnection db = new MySqlConnection(DbConnection.Cadena()))
                 {
                     if (db.State == ConnectionState.Closed)
                         db.Open();
@@ -29,34 +30,43 @@ namespace ResBarLib
             }
             catch
             {
-                throw new ErrorAplicationException("ManejadorProductos.ObtenerxCategoria()$ No es posible conectarse a la DB");
+                throw new ErrorAplicationException("ManejadorParametros.Obtener()$ No es posible conectarse a la DB");
             }
         }
 
+        //Actualiza el valor del parámetro en la base de datos, no se puede actualizar ni el ID, ni el nombre, solo se puede modificar el campo valor
         public static int Actualizar(parametro param)
         {
+            int respuesta = 0;
             try
             {
-                using (IDbConnection db = new MySqlConnection(ConfigurationManager.ConnectionStrings["cn"].ConnectionString))
+                using (IDbConnection db = new MySqlConnection(DbConnection.Cadena()))
                 {
                     if (db.State == ConnectionState.Closed)
                         db.Open();
                     string query = "UPDATE parametro SET nombre='" + param.nombre + "', valor='" + param.valor + "' WHERE idProducto= '" + param.idParametro + "';";
-                    var respuesta = db.Execute(query, param);
-                    return respuesta;
+                    respuesta = db.Execute(query, param);
                 }
 
+                if (respuesta < 0)
+                {
+                    throw new ErrorAplicationException("ManejadorParametros.Actualizar()$No se puede realizar la operación de Actualizar");
+                }
+                else { return respuesta; }
+
             }
-            catch (ErrorAplicationException ex)
+            catch
             {
-                throw new ErrorAplicationException(ex.Message, ex.InnerException);
+                throw new ErrorAplicationException("ManejadorParametros.Actualizar()$Problemas al conectar en la DB");
             }
         }
+
+        //Toma el idParametro y busca en la base de datos una tupla que coincida con dicho ID, luego devuelve un objeto parametro construido acorde a la tupla
         public static parametro Obtener(int idparametro)
         {
             try
             {
-                using (IDbConnection db = new MySqlConnection(ConfigurationManager.ConnectionStrings["cn"].ConnectionString))
+                using (IDbConnection db = new MySqlConnection(DbConnection.Cadena()))
                 {
                     if (db.State == ConnectionState.Closed)
                         db.Open();
@@ -66,9 +76,9 @@ namespace ResBarLib
                 }
 
             }
-            catch (ErrorAplicationException ex)
+            catch
             {
-                throw new ErrorAplicationException(ex.Message, ex.InnerException);
+                throw new ErrorAplicationException("ManejadorParametros.Obtener(int idparametro)$Problemas al conectar en la BD");
             }
         }
     }
